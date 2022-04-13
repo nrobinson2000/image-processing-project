15570514e0724568d4e9eecb5d3eb0b62dbb39f6
Tony Lei <84546094+leixatwit@users.noreply.github.com>
2022-04-13 13:36:19 -0400

    Completed UI Open Image

--- functions/main.m
+++ functions/main.m
@@ -1,7 +1,7 @@
-function main(I)
-    image = imread(I);
+function main(image)
     
     preprocessed_image = preprocess_function(image);
     segment_function(preprocessed_image);
     pred = predict_function();
     disp(pred);
+end
